88a3895ab1e7105d4d306893403c0d8af56c0c9c
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2024-05-05 16:33:32 +0200

    search prob img

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './_navigation.scss';
 import { NavLink } from 'react-router-dom';
+import Logo from './LOGO.svg';
 
 const Navigation = () => {
     return (
         <div className="navigation">
-            <img src="/img/LOGO.svg" alt="Logo de l'agence de locations d'appartements" />
+            <img src={Logo} alt="Logo de l'agence de locations d'appartements" /> 
             <ul>
                 <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <li>Accueil</li>
